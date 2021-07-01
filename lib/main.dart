9947b490872061f13f3b38c387d7edbe8c9fa162
yash <yashwanthkrishna5027@gmail.com>
2021-07-01 16:16:51 +0530

    homepage

--- lib/main.dart
+++ lib/main.dart
@@ -10,17 +10,87 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(),
+      theme: ThemeData(
+          textTheme: TextTheme(
+              headline1: TextStyle(
+                color: Colors.white,
+                fontSize: 35,
+                fontFamily: 'Poppins',
+                fontWeight: FontWeight.w900,
+              ),
+              bodyText1: TextStyle(
+                color: Colors.white,
+                fontFamily: 'Poppins',
+                fontSize: 14,
+                fontWeight: FontWeight.w900,
+              ))),
       home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('images/back.jfif'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        constraints: BoxConstraints.expand(),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            RichText(
+              text: TextSpan(children: [
+                TextSpan(
+                  text: 'Vtrious',
+                  style: Theme.of(context).textTheme.headline1,
+                ),
+                TextSpan(
+                    text: '.',
+                    style: TextStyle(color: Colors.red, fontSize: 35))
+              ]),
+            ),
+            SizedBox(
+              height: 11,
+            ),
+            Container(
+              width: 214,
+              child: Text(
+                'Whether we create history or not we are part ofthe history',
+                textAlign: TextAlign.center,
+                style: Theme.of(context).textTheme.bodyText1,
+              ),
+            ),
+            SizedBox(
+              height: 46,
+            ),
+            MaterialButton(
+              onPressed: () {},
+              child: Text('Get Started',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16,
+                    fontFamily: 'Poppins',
+                    fontWeight: FontWeight.w900,
+                  )),
+              color: Colors.red,
+              padding: EdgeInsets.symmetric(horizontal: 23),
+              height: 70,
+              minWidth: 300,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20)),
+            ),
+            SizedBox(
+              height: 111,
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
